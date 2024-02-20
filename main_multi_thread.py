3cd2b3517b43d2cb90f78fbb4938efde2a116eca
Victor Zhao <victorzhao1999@gmail.com>
2024-02-20 10:14:08 +0000

    Update main_multi_thread.py

--- main_multi_thread.py
+++ main_multi_thread.py
@@ -3,7 +3,15 @@ import multiprocessing
 import sys
 
 # Define the datasets and augmentations
-datasets = ["musae_Twitch_DE", "grand_Brain", "grand_LungCancer", "benchmark_Cora_Cite", "musae_Github"]
+# datasets = ["musae_Facebook", "musae_Github",
+#             "musae_Twitch_FR", "musae_Twitch_EN", "musae_Twitch_ES",
+#             "musae_Twitch_PT", "musae_Twitch_RU", "musae_Twitch_DE",
+#             "musae_Wiki_chameleon", "musae_Wiki_crocodile", "musae_Wiki_squirrel",
+#             "grand_ArteryAorta", "grand_ArteryCoronary", "grand_Breast", "grand_Brain", "grand_Leukemia",
+#             "grand_Lung", "grand_Stomach", "grand_LungCancer", "grand_StomachCancer", "grand_KidneyCancer",
+#             "amazon_Photo", "amazon_Computer", "benchmark_Cora_Author", "benchmark_Cora_Cite", "benchmark_Pubmed"]
+datasets = ["musae_Github", "musae_Twitch_DE", "grand_Brain", "grand_LungCancer", "benchmark_Cora_Cite", 
+            "amazon_Computer"]
 augmentations =  ['NodeDrop', 'EdgeDrop', 'NodeMixUp', 'NodeFeatureMasking','NULL']
 
 # Retrieve the model from the command line arguments
@@ -11,19 +19,23 @@ model = sys.argv[1]
 
 # Function to run the subprocess with output redirected to a file
 def run_process(dataset, augmentation, model):
-    data_dir = f"data_reformatted/{dataset.lower()}"
-    raw_data_dir = f"data_reformatted/raw/"
+    # Cirrus HPC
+    data_dir = f"/work/ec249/ec249/xz9118/Projects/graph-cross-attention/data_reformatted/{dataset.lower()}"
+    raw_data_dir = f"/work/ec249/ec249/xz9118/Projects/graph-cross-attention/data_reformatted/raw/"
+
+    # Local
+    # data_dir = f"data_reformatted/{dataset.lower()}"
+    # raw_data_dir = f"data_reformatted/"
 
     # Construct the command
-    command = f"python train.py --method {model} --dname {dataset} " \
+    command = f"python train.py --method {model} --dname {dataset} --augment {augmentation}" \
               f"--All_num_layers 2 --MLP_num_layers 2 --MLP2_num_layers 2 --MLP3_num_layers 2 " \
               f"--Classifier_num_layers 2 --MLP_hidden 256 --Classifier_hidden 64 --aggregate mean " \
-              f"--restart_alpha 0.5 --lr 0.001 --wd 0 --epochs 500 --runs 2 --feature_noise 1.0 --cuda -1 " \
-              f"--data_dir {data_dir} --raw_data_dir {raw_data_dir} " \
-              f"--Augment {augmentation}"
+              f"--restart_alpha 0.5 --lr 0.001 --wd 0 --epochs 500 --runs 2 --feature_noise 1.0 --cuda 0 " \
+              f"--data_dir {data_dir} --raw_data_dir {raw_data_dir}"
 
     # Run the command and redirect output to a file
-    with open(f'outputs/output_{dataset}_{augmentation}.txt', 'w') as outfile:
+    with open(f'out/{dataset}-{model}-{augmentation}.out', 'w') as outfile:
         subprocess.run(command, shell=True, check=True, stdout=outfile, stderr=subprocess.STDOUT)
 
 # Limit for the number of processes
